01c487db84ed4f1f191d0ab65a54c75e4097ece8
Chris Aquino <chris.aquino@gmail.com>
2019-03-05 11:37:30 -0500

    adds address book example

--- scripts/index.js
+++ scripts/index.js
@@ -31,5 +31,14 @@ function transl33t(originalString) {
 }
 
 const addressBook = {
-    
+    DigitalCrafts: {
+        phone: '404-123-1234', 
+        address: '3423 Piedmont', 
+        city: 'Atlanta',
+    },
+    "Chris Aquino": {
+        phone: '404-999-1234', 
+        address: '3421 Piedmont', 
+        city: 'Atlanta',
+    },
 };
